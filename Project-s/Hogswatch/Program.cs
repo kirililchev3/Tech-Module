8aecefc877ef3c38c68f905ffcddf044fe9a9461
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-11 10:59:19 +0200

    Add files via upload

---
+++ Project-s/Hogswatch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Hogswatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            int numbersOfHomes = int.Parse(Console.ReadLine());
+            int totalTakenPeresents = int.Parse(Console.ReadLine());
+            int timesBack = 0;
+            int additionalPresents = 0;
+            int homeVisiter = 0;
+            int remainingHomes = numbersOfHomes;
+            int totalAddition = 0;
+            int presentLeft = totalTakenPeresents;
+            for (int i = 1; i <= numbersOfHomes; i++)
+            {
+                int numberOfSocks = int.Parse(Console.ReadLine());
+                homeVisiter++;
+                remainingHomes -= 1;
+                if (presentLeft < numberOfSocks)
+                {
+                    additionalPresents = numberOfSocks - presentLeft;
+                    timesBack++;
+                    totalAddition += 
+                        (int)(totalTakenPeresents / homeVisiter) * remainingHomes + additionalPresents;
+                    presentLeft = (totalAddition - additionalPresents);
+                }
+                else
+                {
+                    presentLeft -= numberOfSocks;
+                }
+
+            }
+
+            if (totalAddition <= 0 )
+            {
+                Console.WriteLine(presentLeft);
+            }
+            else
+            {
+                Console.WriteLine(timesBack);
+                Console.WriteLine(totalAddition);
+            }
+
+        }
+    }
+}
