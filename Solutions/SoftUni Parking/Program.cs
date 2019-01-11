8023590cb95379d6c598719257df750fcc345afb
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-11 11:17:17 +0200

    Add files via upload

---
+++ Solutions/SoftUni Parking/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni_Parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dictionary<string, string> parking = new Dictionary<string, string>();
+            int numCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numCommands; i++)
+
+            {
+                string inputLine = Console.ReadLine();
+                var commands = inputLine.Split().ToArray();
+                string command = commands[0];
+                string userName = commands[1];
+                
+                if (command == "register")
+                {
+                    string licenseNumber = commands[2].ToUpper();
+                    
+                    if (parking.ContainsKey(userName))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {parking[userName]}");
+                        
+                    }
+                    else if (!parking.ContainsKey(userName))
+                    {
+                        parking.Add(userName, licenseNumber);
+                        Console.WriteLine($"{userName} registered {licenseNumber} successfully");
+                    }
+
+                }
+                else if (command == "unregister")
+                {
+                    if (!parking.ContainsKey(userName))
+                    {
+                        Console.WriteLine($"ERROR: user {userName} not found");
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{userName} unregistered successfully");
+                        parking.Remove(userName);
+                    }
+                    
+                    
+                }
+               
+            }
+            foreach (var plate in parking)
+            {
+                Console.WriteLine($"{plate.Key} => {plate.Value}");
+            }
+        }
+
+        
+    }
+}
